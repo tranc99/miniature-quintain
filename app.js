c232fe5a4f0ccd2679653b695bc30ba6007bdd43
Ten Mutunhire <tmutunhire@gmail.com>
2014-06-20 17:31:57 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var app = express();
+
+//setup views
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
